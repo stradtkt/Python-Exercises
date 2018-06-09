ee5a18fc554b58ff5b63e25dcbf0de96edd1de4c
Kevin <stradtkt22@gmail.com>
2018-06-09 09:41:00 -0400

    Added unpacking_tuples.py

---
+++ arrays/reverse_array.py
@@ -0,0 +1,4 @@
+from array import *
+array_item = array('i', [1,2,3,4,5,6,7,8,9])
+array_item.reverse()
+print(str(array_item))
---
+++ tuples/unpacking_tuple.py
@@ -0,0 +1,9 @@
+my_tuple = 1,2,3
+print(my_tuple)
+num1 , num2, num3 = my_tuple
+# adding all the numbers in the tuple together
+print(num1 + num2 + num3)
+# each item can be printed out
+print(num1)
+print(num2)
+print(num3)

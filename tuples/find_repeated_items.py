df7cf65885b11c1ccc29753ffa19d4198328ac5d
Kevin <stradtkt22@gmail.com>
2018-06-09 09:46:07 -0400

    Added items to the tuples folder

---
+++ tuples/find_repeated_items.py
@@ -0,0 +1,7 @@
+my_tuple = 2,4,6,2,4,6,5,2,4,6,7
+count1 = my_tuple.count(2)
+count2 = my_tuple.count(4)
+count3 = my_tuple.count(6)
+print(count1)
+print(count2)
+print(count3)
---
+++ tuples/tuple_to_string.py
@@ -0,0 +1,3 @@
+my_tuple = ('H', 'e', 'l', 'l', 'o')
+string = ''.join(my_tuple)
+print(string)
